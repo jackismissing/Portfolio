1704627aaea423d1d44c144d94fc7fc94f40bd19
jackismissing <nicolas.j.garnier@gmail.com>
2013-09-05 23:04:51 +0200

    responsive

--- static/js/main.js
+++ static/js/main.js
@@ -6,8 +6,10 @@ function NicolasMain(delay){
 	
 	var self = this;
 	this._delay = delay;
+	this._mainWrapper = $('.wrapper'); 
 	this._hexagons = $('.hexagons li');
 	this._projectDetailsWrapper = $('#project-details');
+	this._closeBar = $('#close-bar');
 	this._panels = '';
 
 	this._init(self);
@@ -30,13 +32,16 @@ NicolasMain.prototype.listen = function(self){
 
 	this._hexagons.on('click',function(){
 		var hexagon = this;
-		console.log(hexagon);
 		self.showProjectDetails(self, hexagon);
 	});
 
-	this._panels.on('click',function(){
-		self.hideProjectDetails(self);
-	});
+	if(this._closeBar.length)
+	{
+		this._closeBar.on('click',function(){
+			self.hideProjectDetails(self);
+		});
+	}
+	
 };
 
 NicolasMain.prototype.showProjectDetails = function(self, hexagon){
@@ -46,9 +51,11 @@ NicolasMain.prototype.showProjectDetails = function(self, hexagon){
 	this._projectDetailsWrapper.addClass('active');
 	this._panels.addClass('active');
 
-	if(currentProject.length)
+	if(currentProject.length && this._mainWrapper.length)
 	{
 		window.setTimeout(function(){
+
+			self._mainWrapper.addClass('no-display');
 			self._projectDetailsWrapper.prepend(currentProject)
 			var newArticle = self._projectDetailsWrapper.find('article');
 			newArticle.fadeIn(200, function(){
@@ -66,6 +73,11 @@ NicolasMain.prototype.hideProjectDetails = function(self){
 	{
 		currentProject.remove();
 
+	}
+
+	if(this._mainWrapper.length && this._mainWrapper.hasClass('no-display'))
+	{
+		this._mainWrapper.removeClass('no-display');
 	}
 	this._panels.removeClass('active');
 	window.setTimeout(function(){
